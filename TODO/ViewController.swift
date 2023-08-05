e4d0c2b96e1d51288c6bc0b231b80da1922a7009
Yeskendir <yeskendiriskakov@mail.ru>
2023-08-06 01:54:27 +0600

    Initial Commit

---
+++ TODO/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TODO
+//
+//  Created by Yeskendir on 06.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
